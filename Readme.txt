0bdb258d4e56543c6478963f674bf35299339e33
adamtuliper <adam.tuliper@gmail.com>
2015-03-31 23:56:28 -0700

    Initial Commit

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+Visual Studio 2013 basic ASP.NET template
+This will take bower main files, copy to wwwroot/lib for jQuery
+it will then use wiredep to wire up dependencies (scripts) and inject script tags into index.html
+It will smartly determine the correct order to include scripts.
---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+﻿
+module.exports = function (grunt) {
+    grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: "wwwroot/lib",
+                    layout: "byComponent",
+                    cleanTargetDir: false
+                }
+            }
+        },
+        wiredep: {
+
+            task: {
+
+                // Point to the files that should be updated when
+                // you run `grunt wiredep`
+                src: [
+                  '*.html'   // .html support
+                ],
+
+                options: {
+                    // See wiredep's configuration documentation for the options
+                    // you may pass:
+
+                    // https://github.com/taptapship/wiredep#configuration
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-wiredep');
+
+    //  grunt.loadNpmTasks("grunt-contrib-uglify");
+    // This command registers the default task which will install bower packages into wwwroot/lib
+    grunt.registerTask("default", ["bower:install", "wiredep"]);
+
+
+    // The following line loads the grunt plugins.
+    // This line needs to be at the end of this this file.
+    grunt.loadNpmTasks("grunt-bower-task");
+};
